2c0e50a29a37726ae5a14223703ad978ca9ce73d
WangSl <wangsl@stu.hit.edu.cn>
2024-11-04 08:16:08 +0800

    111

--- utils/conf.py
+++ utils/conf.py
@@ -4,7 +4,34 @@ class Configuration:
     def __init__(self, confPath: str = ""):
         self.confPath = confPath
         self.defaultConf = {
+            "data_path": "../Data/Mine/gist/gist.data",
+            "log_path": "./example/example.log",
+            "model_path": "./example/example.pth",
+            "train_path": "./example/data/train.data",
+            "val_path": "./example/data/val.data",
+            "train_indices_path": "./example/data/train_indices.data",
+            "val_indices_path": "./example/data/val_indices.data",
+
+            "epoch_max": 100,
+            "device": "cuda:6",
+            "len_series": 960,
+            "len_reduce": 60,
+            "dim_series": 1,
+            "batch_size": 16,
             
+            "data_size": 1000000,
+            "train_size": 20000,
+            "val_size": 1000,
+
+            "embed_size": 16,
+            "num_tokens": 10000,
+            "num_heads": 4,
+            "num_layers": 6,
+
+            "orth_regularizer": "srip",
+            "srip_mode": "linear",
+            "srip_max": 5e-4,
+            "srip_min": 0
         }
         self.loadConf()
     
